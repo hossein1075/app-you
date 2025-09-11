4684b0eb9e381e49beb2d502c970f42262de9339
Hossein <hosseinh1075@gmail.com>
2025-09-11 21:36:41 +0430

    add css section progress part 1

--- src/Components/main/section2/progress.jsx
+++ src/Components/main/section2/progress.jsx
@@ -7,19 +7,19 @@ function Progress() {
         <>
             <section>
                 <div className="container">
-                    <div className='flex items-center-center'>
-                        <div>
+                    <div className='flex items-center justify-between'>
+                        <div className='max-w-138'>
                         <HalfTitle text={Info[1].text}/>
-                        <h3>برنامه گرت برای همیشه</h3>
-                        <p>
+                        <h3 className='text-neutral-700 max-md:text-[22px] text-[25px]'>برنامه گرت برای همیشه</h3>
+                        <p className='max-md:text-sm text-slate-400'>
                             لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
                             بیشتر بدانید
                         </p>
                         <ButtonInfo/>
                     </div>
-                    <div className='bg-progress'>
+                    <div className='block max-xl:hidden bg-progress max-w-138'>
                         <div>
-                            <img src="/public/images/progress-phone.png" alt="progress" />
+                            <img src="/public/images/progress-phone.png" className='max-lg:hidden block' alt="progress" />
                         </div>
                     </div>
                     </div>
