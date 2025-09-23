bb9a580e11a5126f9de7c8b4ab52b6ad0fe06a09
Hossein <hosseinh1075@gmail.com>
2025-09-24 00:23:17 +0330

    add style section features

--- src/Components/main/boxIcon/Boxicon.jsx
+++ src/Components/main/boxIcon/Boxicon.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function Boxicon() {
   return (
     <>
-    <div className='flex bg-sky-300 gap-4'>
+    <div className='flex gap-4 mb-15'>
         <span className='size-17.5 rounded-full bg-white flex justify-center items-center text-blue-500  hover:bg-gradient-to-tr from-grandiant-200 to-grandiant-100 hover:text-white  transition-colors duration-500'>
-            <i className="lnr lnr-rocket text-xl"></i>
+            <i className="lnr lnr-rocket text-xl leading-none"></i>
         </span>
         <div className='text-white'>
             <h4 className='mb-2.5'>سریع و قدرتمند</h4>
-            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
+            <p className='title-thd'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</p>
         </div>
     </div>
     </>
--- src/Components/main/section4/feature.jsx
+++ src/Components/main/section4/feature.jsx
@@ -2,28 +2,33 @@ import React from 'react'
 import FullTitle from '../fullTitle/fullTitle'
 import Info from '../info'
 import Boxicon from '../boxIcon/Boxicon'
+import './feature.css'
 function Feature() {
   return (
     <>
-    <section>
+      <section className='text-white bg-blue-500 pt-20'>
         <div className="container">
-        <FullTitle text={Info[3].text}/>
-        <h3>ویژگی های پر قدرت مثل همیشه</h3>
-        <div>
-          <Boxicon/>
-          <Boxicon/>
-          <Boxicon/>
+          <div className='mx-auto text-center'>
+            <FullTitle text={Info[3].text}/>
+            <h3 className='mt-2.5 mb-18.75 text-[25px]'>ویژگی های پر قدرت مثل همیشه</h3>
+          </div>
+         <div className='flex max-md:flex-col flex-row justify-center'>
+           <div className='flex-shrink-0'>
+            <Boxicon />
+            <Boxicon />
+            <Boxicon />
+          </div>
+          <div className=''>
+            <img src="public/images/feature-image.png" className='max-lg:hidden block xl:max-w-90' alt="feature" />
+          </div>
+          <div className='flex-shrink-0'>
+            <Boxicon />
+            <Boxicon />
+            <Boxicon />
+          </div>
+         </div>
         </div>
-        <div>
-            <img src="public/images/feature-image.png" alt="feature" />
-        </div>
-        <div>
-          <Boxicon/>
-          <Boxicon/>
-          <Boxicon/>
-        </div>
-        </div>
-    </section>
+      </section>
     </>
   )
 }
