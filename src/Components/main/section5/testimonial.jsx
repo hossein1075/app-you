5e6aba3f1f4788a7ed452f3f2b92f8a10db96ede
Hossein <hosseinh1075@gmail.com>
2025-09-25 18:52:45 +0330

    add slider section feature

--- src/Components/main/section5/testimonial.jsx
+++ src/Components/main/section5/testimonial.jsx
@@ -1,32 +1,80 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import FullTitle from '../fullTitle/fullTitle'
 import Info from '../info'
 import ImageSlide from '../infoSlide'
 import Slide from '../slide/Slide'
+import './testimonial.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 function Testimonial() {
+  const swiperRef = useRef(null);
   return (
     <>
       <section>
-        <div className="container">
-          <div className='mx-auto text-center'>
-            <FullTitle text={Info[3].text} />
-            <h3 className='mt-2.5 mb-18.75 text-[25px] max-md:text-22'>ویژگی های پر قدرت مثل همیشه</h3>
-          </div>
+        <div className="container mx-auto text-center">
+          <FullTitle text={Info[3].text} />
+          <h3 className='mt-2.5 mb-18.75 text-[25px] max-md:text-22'>ویژگی های پر قدرت مثل همیشه</h3>
 
-          <div className='flex items-center'>
-            <div className='size-17.5 shrink-0 rounded-full bg-white z-20 flex justify-center items-center shadow-[0_0_30px_0_rgba(0,0,0,0.1)] text-blue-500  hover:bg-gradient-to-tr from-grandiant-200 to-grandiant-100 hover:text-white  transition-colors duration-500'>
+          <div className=' flex items-center mt-10'>
+            {/* دکمه قبلی */}
+            <div
+              onClick={() => swiperRef.current?.slidePrev()}
+              className='max-lg:hidden size-17.5 shrink-0 rounded-full bg-white z-20 flex justify-center items-center shadow-[0_0_30px_0_rgba(0,0,0,0.1)] text-blue-500 hover:bg-gradient-to-tr from-grandiant-200 to-grandiant-100 hover:text-white transition-colors duration-500 cursor-pointer'
+            >
               <i className="lnr lnr-chevron-right text-xl leading-none"></i>
             </div>
-            <div className='grid grid-cols-3'>
-              <Slide img={ImageSlide[0].img} />
-              <Slide img={ImageSlide[1].img} />
-              <Slide img={ImageSlide[2].img} />
-            </div>
-            <div className='size-17.5 shrink-0 rounded-full bg-white z-20 flex justify-center items-center shadow-[0_0_30px_0_rgba(0,0,0,0.1)] text-blue-500  hover:bg-gradient-to-tr from-grandiant-200 to-grandiant-100 hover:text-white  transition-colors duration-500'>
+
+            <Swiper
+              modules={[Navigation,]}
+
+              slidesPerView={3}
+              loop={true}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              onSwiper={(swiper) => (swiperRef.current = swiper)}
+              className="mx-4 flex-1"
+              breakpoints={{
+                200: { slidesPerView: 1 },
+                560: { slidesPerView: 1 },
+                768: { slidesPerView: 2 },
+                1024: { slidesPerView: 3 },
+              }}
+            >
+              {ImageSlide.map((item, idx) => (
+                <SwiperSlide key={idx}>
+                  <Slide img={item.img} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+
+            {/* دکمه بعدی */}
+            <div
+              onClick={() => swiperRef.current?.slideNext()}
+              className='max-lg:hidden size-17.5 shrink-0 rounded-full bg-white z-20 flex justify-center items-center shadow-[0_0_30px_0_rgba(0,0,0,0.1)] text-blue-500 hover:bg-gradient-to-tr from-grandiant-200 to-grandiant-100 hover:text-white transition-colors duration-500 cursor-pointer'
+            >
               <i className="lnr lnr-chevron-left text-xl leading-none"></i>
             </div>
           </div>
-
+          <div className='flexcenter'>
+            {/* دکمه قبلی */}
+            <div
+              onClick={() => swiperRef.current?.slidePrev()}
+              className='size-17.5 mx-3.75 mb-3.75 mt-7.5 shrink-0 rounded-full bg-white z-20 max-lg:flex hidden justify-center items-center shadow-[0_0_30px_0_rgba(0,0,0,0.1)] text-blue-500 hover:bg-gradient-to-tr from-grandiant-200 to-grandiant-100 hover:text-white transition-colors duration-500 cursor-pointer'
+            >
+              <i className="lnr lnr-chevron-right text-xl leading-none"></i>
+            </div>
+            {/* دکمه بعدی */}
+            <div
+              onClick={() => swiperRef.current?.slideNext()}
+              className='size-17.5 mx-3.75 mb-3.75 mt-7.5 shrink-0 rounded-full bg-white z-20 max-lg:flex hidden justify-center items-center shadow-[0_0_30px_0_rgba(0,0,0,0.1)] text-blue-500 hover:bg-gradient-to-tr from-grandiant-200 to-grandiant-100 hover:text-white transition-colors duration-500 cursor-pointer'
+            >
+              <i className="lnr lnr-chevron-left text-xl leading-none"></i>
+            </div>
+          </div>
         </div>
       </section>
     </>
--- src/Components/main/slide/Slide.jsx
+++ src/Components/main/slide/Slide.jsx
@@ -3,14 +3,16 @@ import React from 'react'
 function Slide(props) {
   return (
     <>
-     <div className='relative px-7.5 pb-7.5 pt-13.75 rounded-bl-[5px] shadow-[0_0_30px_-5px_rgba(0,0,0,0.1)] hover:shadow-[0_0_10px_0_rgba(0,0,0,0.1)] mx-3.75'>
+<div className=' mx-3.75 h-[360px] flex flex-col justify-center'>
+       <div className='relative px-7.5 pb-7.5 pt-13.75 rounded-bl-[5px] shadow-[0_0_30px_-5px_rgba(0,0,0,0.1)] hover:shadow-[0_0_10px_0_rgba(0,0,0,0.1)]'>
             <h4 className='text-blue-500 font-semibold text-xl mb-3.75'>ملیکا خلیلیان</h4>
             <h6 className='text-sm/[1.4rem] mb-3.75 text-slate-400'>کارگردان هنری</h6>
             <p className='text-slate-400 mb-2.5'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است</p>
-            <div className='absolute top-[-40px] right-10px size-20 rounded-full bg-white border-[5px] border-solid border-white shadow-[0_0_10px_0_rgba(0,0,0,0.1)]'>
-              <img src={props.img} alt="team" />
+            <div className='absolute top-[-40px] right-10px z-50 size-20 rounded-full bg-white border-[5px] border-solid border-white shadow-[0_0_10px_0_rgba(0,0,0,0.1)]'>
+              <img src={props.img} className='' alt="team" />
             </div>
           </div>
+</div>
     </>
   )
 }
