756912f3ed541e6eba8a7f79bee6c772e632c422
Hossein <hosseinh1075@gmail.com>
2025-09-27 17:26:46 +0330

    complete section gallery

--- src/Components/main/section5/testimonial.jsx
+++ src/Components/main/section5/testimonial.jsx
@@ -31,6 +31,7 @@ function Testimonial() {
 
               slidesPerView={3}
               loop={true}
+              speed={1000}
               autoplay={{
                 delay: 3000,
                 disableOnInteraction: false,
--- src/Components/main/section6/gallery.jsx
+++ src/Components/main/section6/gallery.jsx
@@ -1,44 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HalfTitle from '../halfTitle/halfTitle'
 import ButtonInfo from '../buttonInfo/buttonInfo'
 import Info from '../info'
 import gallerySImage from '../slide/gallerySlide'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Autoplay } from 'swiper/modules';
+import { Navigation, Autoplay, Thumbs } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
 function Gallery() {
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
     return (
         <>
             <section className='text-white bg-gradient-to-tr from-grandiant-200 to-grandiant-100 max-md:py-15 py-30'>
                 <div className="container">
-                    <div className='max-md:flex-col flex gap-x-15'>
-                        <div className='w-200'>
+                    <div className='grid max-md:grid-cols-1 grid-cols-2 gap-x-15'>
+                        <div className='w-full max-md:max-w-100 mx-auto relative'>
                             <Swiper
-                                modules={[Navigation, Autoplay]}
-                                slidesPerView={4}
+                                modules={[Navigation, Autoplay, Thumbs]}
+                                thumbs={{ swiper: thumbsSwiper }}
+                                slidesPerView={1}
+                                spaceBetween={8}
+                                speed={1000}
                                 loop={true}
-                                  dir="ltr"  
+                                dir="ltr"
                                 autoplay={{
                                     delay: 3000,
                                     disableOnInteraction: false,
                                 }}
-                                
-                                className=""
+
+                                pagination={{
+                                    clickable: true,
+                                    dynamicBullets: true,
+                                }}
                                 breakpoints={{
-                                    200: { slidesPerView: 1 },
+
                                     768: { slidesPerView: 2 },
-                                    1024: { slidesPerView: 4 },
+                                    1280: { slidesPerView: 2 },
+                                    1536: { slidesPerView: 4 },
                                 }}
                             >
                                 {gallerySImage.map((item, idx) => (
-                                    <SwiperSlide key={idx}>
-                                          <img src={item.img} alt={`gallery-${item.id}`} className="w-37"/>
+
+                                    <SwiperSlide key={idx} className=''>
+                                        <div className='max-md:flex justify-center'>
+                                            <img src={item.img} alt={`gallery-${item.id}`} className="" />
+                                        </div>
                                     </SwiperSlide>
                                 ))}
                             </Swiper>
+                            <Swiper
+                                onSwiper={setThumbsSwiper}
+                                slidesPerView={4}
+                                spaceBetween={10}
+                                freeMode={true}
+                                watchSlidesProgress={true}
+                                modules={[Thumbs, Navigation]}
+                                className="thumbsSwiper mt-4"
+                            >
+                                {gallerySImage.map((item, idx) => (
+                                    <SwiperSlide key={idx} className="cursor-pointer">
+                                        <img src={item.img} alt={`thumb-${item.id}`} className="w-full h-20 object-cover rounded max-sm:hidden" />
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+
                         </div>
-                        <div className=' max-w-138'>
+                        <div className='w-full md:w-[360px] mt-6 md:mt-0'>
                             <HalfTitle text={Info[5].text} className='mb-3.75' color={'white'} />
                             <h3 className=' max-md:text-[22px] text-[25px] pt-2.5 pb-3.75'>اسکرین شات 01</h3>
                             <p className='max-md:text-sm pt-5 pb-12.5 max-w-114 max-xl:max-w-full'>
