d0504b7ed85d354bfb65200b1e8b284c7ddbddd7
Hossein <hosseinh1075@gmail.com>
2025-10-03 12:32:48 +0330

    add & complete nav pagination

--- src/Components/blog/paginationSec/PaginationSec.jsx
+++ src/Components/blog/paginationSec/PaginationSec.jsx
@@ -3,14 +3,36 @@ import BoxBlog from '../articleSite/boxBlog/BoxBlog'
 import InfoBoxBlog from '../articleSite/boxBlog/InfoBoxBlog'
 import './Pagination.css'
 function PaginationSec() {
-  
+    const [currnetPage, setCurrentPage] = useState(1)
+    const [pagination, setPagination] = useState([])
+
+    let pageSize = 3
+    let pageNumber
+
+    useEffect(()=> {
+        let endIndex = pageSize * currnetPage
+        let firstIndex = endIndex - pageSize
+        let allPage = InfoBoxBlog.slice(firstIndex, endIndex)
+        setPagination(allPage)
+    }, [currnetPage])
+
+    let pageCount = Math.ceil(InfoBoxBlog.length / pageSize)
+    pageNumber = Array.from(Array(pageCount).keys())
+
+    const changePage = (newPage) => {
+        setCurrentPage(newPage)
+        let endIndex = pageSize * newPage
+        let firstIndex = endIndex - pageSize
+        let allPage = InfoBoxBlog.slice(firstIndex, endIndex)
+        setPagination(allPage)
+    }
 
     return (
         <>
             <section>
                 <div className="container">
                     <div className='grid max-sm:grid-cols-1 max-lg:grid-cols-2 grid-cols-3 gap-5 px-5'>
-                        {InfoBoxBlog.map(item => (
+                        {pagination.map(item => (
                             <BoxBlog
                                 key={item.id}
                                 img={item.img}
@@ -18,7 +40,20 @@ function PaginationSec() {
                         ))}
                     </div>
 
-                  
+                    <nav className='text-center mt-20 p-5 shadow-[0_0_15px_0_rgba(0,0,0,0.1)] flex items-center gap-3'>
+
+                        <button href="#" disabled={currnetPage === 1} className={`size-10 flexcenter rounded-[50%] inline-block hover:text-indigo-700 hover:bg-indigo-50`} onClick={()=> changePage(currnetPage - 1)}>
+                            <span className='lnr lnr-chevron-right'></span>
+                        </button>
+                        {pageNumber.map(item => (
+                            <button href="#" className={`${item + 1 === currnetPage ? 'active' : ''} size-10 flexcenter rounded-[50%] inline-block hover:text-indigo-700 hover:bg-indigo-50`} onClick={()=> changePage(item + 1)}>
+                                <span className=''>{item + 1}</span>
+                            </button>
+                        ))}
+                        <button href="#" disabled={currnetPage === pageCount} className={`size-10 flexcenter rounded-[50%] inline-block hover:text-indigo-700 hover:bg-indigo-50`} onClick={()=> changePage(currnetPage + 1)}>
+                            <span className='lnr lnr-chevron-left'></span>
+                        </button>
+                    </nav>
                 </div>
             </section>
         </>
